73991926ad3f3b6d5262d7212f980fb827081074
igicava <alexanderurchenco.ru@gmail.com>
2024-10-12 20:15:44 +0300

    Базовая работа с websocket

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module quetzalcoatl
 
 go 1.22.6
 
-require github.com/mattn/go-sqlite3 v1.14.23
+require github.com/mattn/go-sqlite3 v1.14.24
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- http/server/server.go
+++ http/server/server.go
@@ -4,15 +4,21 @@ import (
 	"net/http"
 
 	"quetzalcoatl/http/handler"
+	"quetzalcoatl/service"
 )
 
 func RunHTTPServer() {
 	r := http.NewServeMux() // Main router
+	hub := service.NewHub()
+	go hub.Run()
 
 	r.HandleFunc("/getmsg", handler.GetMessage)
 	r.HandleFunc("/setonline", handler.OnlineDevice)
 	r.HandleFunc("/reg", handler.Registration)
 	r.HandleFunc("/msgs", handler.GetMessagesForClient)
+	r.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		service.ServeWs(hub, w, r)
+	})
 
 	http.ListenAndServe(":8888", r)
 }
